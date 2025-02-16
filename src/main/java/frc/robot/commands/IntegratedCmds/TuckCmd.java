4a8d5aaa22e9ea74b1b36f8123dcfee440835d09
KeaniKina <keani1028@gmail.com>
2025-02-16 13:03:25 -1000

    Synced Tuck

--- src/main/java/frc/robot/commands/IntegratedCmds/TuckCmd.java
+++ src/main/java/frc/robot/commands/IntegratedCmds/TuckCmd.java
@@ -1,12 +1,19 @@
 package frc.robot.commands.IntegratedCmds;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.AlgaePivotCmds.NoAlgaeTuckCmd;
 import frc.robot.commands.ElevatorCmds.BottomElevPos;
+import frc.robot.commands.ElevatorCmds.ElevProcessorPos;
 import frc.robot.subsystems.AlgaeIntakeSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 
 public class TuckCmd extends SequentialCommandGroup {
-  public TuckCmd() {
-    addCommands();
+  AlgaeIntakeSubsystem algaeIntakeSub;
+  ElevatorSubsystem elevatorSub;
+  // test comment
+  public TuckCmd(AlgaeIntakeSubsystem newAlgaeIntakeSub, ElevatorSubsystem newElevatorSub) {
+    algaeIntakeSub = newAlgaeIntakeSub;
+    elevatorSub = newElevatorSub;
+    addCommands(new ElevProcessorPos(elevatorSub), new NoAlgaeTuckCmd(algaeIntakeSub), new BottomElevPos(elevatorSub));
   }
 }
