c1c6797b696cec74f691305b5dd4a04709bc3438
JairusTumacder <tumacderjairus7@gmail.com>
2025-02-16 12:46:51 -1000

    added the tuck (no algae and with algae) and processor scoring positions "written"

--- src/main/java/frc/robot/commands/AlgaePivotCmds/ProcessorPositionCmd.java
+++ src/main/java/frc/robot/commands/AlgaePivotCmds/ProcessorPositionCmd.java
@@ -4,46 +4,28 @@
 
 package frc.robot.commands.AlgaePivotCmds;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.AlgaeIntakeSubsystem;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+//Algae Pivot: Processor Position and Pick Up Off the Ground Position are the same position
 public class ProcessorPositionCmd extends Command {
   AlgaeIntakeSubsystem algaeIntakeSub;
-  Timer timer;
 
   public ProcessorPositionCmd(AlgaeIntakeSubsystem newAlgaeIntakeSub) {
     algaeIntakeSub = newAlgaeIntakeSub;
-    timer = new Timer();
     addRequirements(algaeIntakeSub);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    timer.reset();
-    algaeIntakeSub.setSetpoint(-1000); //-1350
+    algaeIntakeSub.setSetpoint(-1000);
     algaeIntakeSub.enablePID();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // if (algaeIntakeSub.isDone()){
-    //   timer.start();
-    //   if (timer.get() >= 1){
-    //     double newOutput = algaeIntakeSub.getOutput();
-    //     algaeIntakeSub.disablePID();
-    //     algaeIntakeSub.setOutput(newOutput);
-    //     timer.stop();
-    //     timer.reset();
-    //   }
-    // }
-    // else{
-    //   algaeIntakeSub.enablePID();
-    //   timer.reset();
-    // }
   }
 
   // Called once the command ends or is interrupted.
