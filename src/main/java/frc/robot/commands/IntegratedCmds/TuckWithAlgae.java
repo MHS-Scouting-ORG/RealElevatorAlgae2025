b4994cc81e88dbd123111a7ea2dc6e96521329e7
JairusTumacder <tumacderjairus7@gmail.com>
2025-02-16 12:53:29 -1000

    test comment

--- src/main/java/frc/robot/commands/IntegratedCmds/TuckWithAlgae.java
+++ src/main/java/frc/robot/commands/IntegratedCmds/TuckWithAlgae.java
@@ -5,15 +5,22 @@
 package frc.robot.commands.IntegratedCmds;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.AlgaePivotCmds.AlgaeTuckCmd;
+import frc.robot.commands.ElevatorCmds.ElevProcessorPos;
+
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.AlgaeIntakeSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class TuckWithAlgae extends SequentialCommandGroup {
-  /** Creates a new TuckWithAlgae. */
-  public TuckWithAlgae() {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+  AlgaeIntakeSubsystem algaeIntakeSub;
+  ElevatorSubsystem elevatorSub;
+  // testing comment
+  public TuckWithAlgae(AlgaeIntakeSubsystem newAlgaeIntakeSub, ElevatorSubsystem newElevatorSub) {
+    algaeIntakeSub = newAlgaeIntakeSub;
+    elevatorSub = newElevatorSub;
+    addCommands(new ElevProcessorPos(elevatorSub), new AlgaeTuckCmd(algaeIntakeSub));
   }
 }
